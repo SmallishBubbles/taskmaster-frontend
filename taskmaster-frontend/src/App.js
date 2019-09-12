0dd0990c08e6e15389f667b0a22e69501010d2cf
MarishaHoza <marisha.hoza@gmail.com>
2019-09-12 11:29:50 -0700

    app:

--- taskmaster-frontend/src/App.js
+++ taskmaster-frontend/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import './reset.css';
 import './App.css';
 
+const API = "http://taskmaster-backend.us-west-2.elasticbeanstalk.com/api/v1"
+
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+
+  function _getTasks() {
+    fetch(API + "/tasks")
+      .then( data => data.json() )
+      .then( fetchedTasks => setTasks(fetchedTasks) );
+  }
+
+  useEffect( _getTasks, [] );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav>
+        <h1>Taskmaster</h1>
+      </nav>
+      <main>
+        <ul>
+          {tasks.map( (task,idx) => {
+            return (
+              <li key={task.id}>
+                <details>
+                  <summary className="flex summary-container">
+                    <div className="summary">
+                      <h2>{task.title}</h2>
+                      <p>{task.description}</p>
+                    </div>
+                  </summary>
+                  <History history={task.history} />
+                </details>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
     </div>
   );
+}
+
+function History(props) {
+  return (
+    <ol className="details">
+      {props.history.map( (record,idx) => {
+        return (
+          <li key={idx}>
+            <span>{record.action} @ </span>
+            <span>{record.time} </span>
+          </li>
+        )
+      })}
+    </ol>
+  )
 }
 
 export default App;
